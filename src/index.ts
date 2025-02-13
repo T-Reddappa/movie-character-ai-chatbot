bbaa772580a54c8c861874f3a1ea1e035bb9139a
Reddappa <itsme.reddy3001@gmail.com>
2025-02-13 16:51:56 +0530

    feat: added express-rate-limit middleware to limit request rate

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
-import express from "express";
+import express, { Request } from "express";
 import cors from "cors";
+import { rateLimit } from "express-rate-limit";
 
 import chatRoutes from "./routes/chat";
 import { PORT } from "./config";
@@ -11,8 +12,19 @@ app.use(cors());
 
 connectDB();
 
-app.use("/chat", chatRoutes);
+//rate limiting(5 requests/sec per user)
+const rateLimiter = rateLimit({
+  windowMs: 1000,
+  limit: 5,
+  message: { error: "Too many requests, please try again later" },
+  keyGenerator: (req: Request) => req.ip || "unknown",
+  standardHeaders: true,
+  legacyHeaders: false,
+});
 
+app.use(rateLimiter);
+
+app.use("/chat", chatRoutes);
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
